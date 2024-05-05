bd8cae8911bbd63b53d452ff031b659a5c64fbe0
unknown <md.monirulmunna.cse@gmail.com>
2024-05-05 11:55:08 +0600

    delete vercel configuration files

--- rcn_back_end/settings.py
+++ rcn_back_end/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = 'django-insecure-u9o^r7d1fliwov=wt7qt0$1!-)xz#k3b@9udhw^7mv61!88rd2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
-# ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -80,12 +80,12 @@ WSGI_APPLICATION = 'rcn_back_end.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # DATABASES = {
 #     'default': {
