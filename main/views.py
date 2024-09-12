741ba9e4be113d3408b91f16ace2ab5da7dfa803
unknown <md.monirulmunna.cse@gmail.com>
2024-09-12 11:13:26 +0600

    changes in customer login view

--- main/views.py
+++ main/views.py
@@ -304,24 +304,70 @@ class TagProductList(generics.ListCreateAPIView):
         return qs
     
 
+# @csrf_exempt
+# def CustomerLogin(request):
+#     username = request.POST.get('username')
+#     password = request.POST.get('password')
+#     user = authenticate(username=username,password=password)
+#     if user:
+#         customer = Customer.objects.get(user=user)
+#         msg = {
+#         'bool': True,
+#         'user': user.username,
+#         'id': customer.id
+#         }
+#     else:
+#         msg = {
+#             'bool':False,
+#             'msg':'Invalid username or password !!'
+#         }
+#     return JsonResponse(msg)
+
+
 @csrf_exempt
 def CustomerLogin(request):
-    username = request.POST.get('username')
-    password = request.POST.get('password')
-    user = authenticate(username=username,password=password)
-    if user:
-        customer = Customer.objects.get(user=user)
-        msg = {
-        'bool': True,
-        'user': user.username,
-        'id': customer.id
-        }
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+        except json.JSONDecodeError:
+            return JsonResponse({'bool': False, 'msg': 'Invalid Username or Password'})
+
+        username = data.get('username')
+        password = data.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            try:
+                customer = Customer.objects.get(user=user)
+
+                # Generate JWT tokens for the authenticated user
+                refresh = RefreshToken.for_user(user)
+                access_token = str(refresh.access_token)
+                refresh_token = str(refresh)
+
+                # Return success message with tokens and customer info
+                msg = {
+                    'bool': True,
+                    'user': user.username,
+                    'id': customer.id,
+                    'access_token': access_token,  # JWT access token
+                    'refresh_token': refresh_token,  # JWT refresh token
+                }
+            except Customer.DoesNotExist:
+                msg = {
+                    'bool': False,
+                    'msg': 'Customer not found for this user!'
+                }
+        else:
+            msg = {
+                'bool': False,
+                'msg': 'Invalid username or password!'
+            }
+
+        return JsonResponse(msg)
     else:
-        msg = {
-            'bool':False,
-            'msg':'Invalid username or password !!'
-        }
-    return JsonResponse(msg)
+        return JsonResponse({'bool': False, 'msg': 'Invalid request method. Only POST is allowed.'})
 
 
 
